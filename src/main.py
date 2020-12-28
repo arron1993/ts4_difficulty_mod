c9998992825dfa167d8b1ea60a7ee0819e30d010
Arron Moore <arron.moore93@gmail.com>
2020-12-28 23:18:03 +0000

    Added flexmethod

--- src/main.py
+++ src/main.py
@@ -1,5 +1,13 @@
+import types
+
 import sims4.commands
 
+import careers
+
+from singletons import DEFAULT
+from sims4.utils import flexmethod
+
+@flexmethod
 def get_hourly_pay(cls, inst, sim_info=DEFAULT, career_track=DEFAULT, career_level=DEFAULT, overmax_level=DEFAULT):
     inst_or_cls = inst if inst is not None else cls
     sim_info = sim_info if sim_info is not DEFAULT else inst.sim_info
@@ -16,7 +24,7 @@ def get_hourly_pay(cls, inst, sim_info=DEFAULT, career_track=DEFAULT, career_lev
     hourly_pay = int(hourly_pay)
     return hourly_pay / 2
 
-careers.career_base.CareerBase.get_hourly_pay = get_hourly_pay
+careers.career_base.CareerBase.get_hourly_pay = types.MethodType(get_hourly_pay, careers.career_base.CareerBase)
 
 @sims4.commands.Command('hellow', command_type=sims4.commands.CommandType.Live)
 def _hellow(_connection=None):
