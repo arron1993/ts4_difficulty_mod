25474f211ca0b02e26f5c85fced323893284188b
Arron Moore <arron.moore93@gmail.com>
2020-12-28 19:11:53 +0000

    Added build script

---
+++ build.py
@@ -0,0 +1,11 @@
+import py_compile
+
+from zipfile import PyZipFile, ZIP_STORED
+from pathlib import Path
+
+def main():
+    for file_ in Path('src').rglob('*.py'):
+        py_compile.compile(file_)
+
+if __name__ == '__main__':
+    main()
