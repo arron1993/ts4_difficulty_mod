718e5d7dad029a80ca3f94d2629ee7845368f0f6
Arron Moore <arron.moore93@gmail.com>
2020-12-28 19:17:25 +0000

    Added build

--- build.py
+++ build.py
@@ -1,11 +1,15 @@
 import py_compile
+import os
 
 from zipfile import PyZipFile, ZIP_STORED
 from pathlib import Path
 
 def main():
     for file_ in Path('src').rglob('*.py'):
-        py_compile.compile(file_)
+        py_compile.compile(
+            file_,
+            os.path.join("build", file_.with_suffix(".pyc"))
+        )
 
 if __name__ == '__main__':
     main()
